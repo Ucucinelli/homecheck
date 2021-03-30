f06c2cb15f7441337bda6956664d05dd3e2fb7e6
Umberto <u.cucinelli@ecubit.it>
2021-03-30 19:13:20 +0200

    new paziente

---
+++ src/main/java/it/ecubit/homecheck/web/PatientController.java
@@ -0,0 +1,61 @@
+package it.ecubit.homecheck.web;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+import it.ecubit.homecheck.service.UserService;
+import it.ecubit.homecheck.web.dto.UserRegistrationDto;
+import it.ecubit.pse.api.dtos.AslVtPatientUserDTO;
+import it.ecubit.pse.api.exceptions.PSEServiceException;
+import it.ecubit.pse.api.interfaces.AslVtPatientServiceInterface;
+import it.ecubit.pse.mongo.entities.AslVtPatient;
+import it.ecubit.pse.mongo.entities.PSEUser;
+
+@Controller
+@RequestMapping("/patients")
+public class PatientController {
+
+    @Autowired
+    private UserService userService;
+    
+    @Autowired
+	private AslVtPatientServiceInterface aslVtPatientService;
+
+    @ModelAttribute("user")
+    public UserRegistrationDto userRegistrationDto() {
+        return new UserRegistrationDto();
+    }
+
+    @GetMapping
+    public String showRegistrationForm(Model model) {
+        return "registration";
+    }
+
+    @PostMapping
+    public String registerUserAccount(@ModelAttribute("patient") @Valid AslVtPatientUserDTO patientDTO,
+                                      BindingResult result) throws PSEServiceException {
+
+        PSEUser existing = userService.findByEmail(patientDTO.getEmail());
+        if (existing != null){
+            result.rejectValue("email", null, "There is already an account registered with that email");
+        }
+
+        if (result.hasErrors()){
+            return "registration";
+        }
+        AslVtPatient newPatient = patientDTO.getPatient();
+
+        aslVtPatientService.save(newPatient);
+        return "redirect:/registration?success";
+    }
+
+}
